68cc8fdb9a51724dfbb27147ac4e64241aa8e7db
Neil Seejoor <nvseejoor@gmail.com>
2018-10-17 19:17:54 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const element = retrieveEmployeeInformation()
+  return document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${element}</li>`)
+}
+
+function addNewLiOnClick() {
+  document.getElementsByTagName('input')[1].addEventListener('click', addNewElementAsLi())
+  document.getElementsByTagName('input')[0].value = ''
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.getElementsByTagName('a')[0].addEventListener('click', () => {
+    document.querySelector('.employee-list').innerHTML = ''
+  })
+}
